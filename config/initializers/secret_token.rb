b4dbfff17ea42e83c48a6d56404e0da5a1abf3ab
Jarek <banyaai[at]gmail.com>
2012-02-18 13:15:57 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fhtelektro::Application.config.secret_token = '1b9ae42f0b3ea64d28ce834d7adfb396ba8fbe102a796bd05f969a0c6088742b0d63089953960ebeb3383d827ed6e8f95e8bbd38b857a77afbc4d2f5b7ff3387'
